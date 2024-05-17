bcb53eefd993aaee2d45b735e3bd5071aa845ab3
vvyejivv <kim_yeji97@naver.com>
2024-05-17 12:14:02 +0900

    240517 로그인 파일 생성

---
+++ lib/login/Login_main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: MyWidget()
+    );
+  }
+}
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+      bottomNavigationBar: BottomAppBar(),
+    );
+  }
+}
+
