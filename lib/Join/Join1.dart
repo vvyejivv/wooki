a07895150c5de35f107fb175aa41a00a6d501d88
jh <jih9117@naver.com>
2024-05-17 18:36:39 +0900

    240517 teamProject

--- lib/Join/Join1.dart
+++ lib/Join/Join1.dart
@@ -1,21 +1,30 @@
+import 'dart:math';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server.dart';
 
-class joinEx2 extends StatefulWidget {
-  const joinEx2({super.key});
+class JoinEx2 extends StatefulWidget {
+  const JoinEx2({Key? key}); // super.key -> Key? key로 수정
 
   @override
-  join createState() => join();
+  _JoinState createState() => _JoinState();
 }
 
-class join extends State<joinEx2> {
-  final FirebaseFirestore _fs = FirebaseFirestore.instance;//FirebaseFirestore에 저장할 변수
+class _JoinState extends State<JoinEx2> {
+
+  final FirebaseFirestore _fs = FirebaseFirestore.instance;
   final TextEditingController _name = TextEditingController();
   final TextEditingController _id = TextEditingController();
   final TextEditingController _pwd = TextEditingController();
   final TextEditingController _pwd1 = TextEditingController();
   final TextEditingController _email = TextEditingController();
   final TextEditingController _phone = TextEditingController();
+
+  late String randomCode = _generateRandomCode();
 
   void _register() async {
     if (_pwd.text != _pwd1.text) {
@@ -25,7 +34,6 @@ class join extends State<joinEx2> {
       return;
     }
 
-    //id 중복체크
     var checkId = await _fs.collection('USERLIST')
         .where('id', isEqualTo: _id.text)
         .where('pwd', isEqualTo: _pwd.text).get();
@@ -62,71 +70,123 @@ class join extends State<joinEx2> {
     }
   }
 
+  final String serverUrl = 'http://10.0.2.2:4000/send-sms';
+
+
+  void sendSMS() async {
+
+    Map<String, String> data = {
+      'to': _phone.text,  // 전달받은 전화번호 사용
+      'from': '01046548947', // 고정
+      'text': '인증 코드: $_generateRandomCode()' // 인증에 사용할 경우 난수 6자리
+    };
+    try {
+      final response = await http.post(
+        Uri.parse(serverUrl),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(data),
+      );
+
+      print('Response status: ${response.statusCode}');
+      print('Response body: ${response.body}');
+    } catch (e) {
+      print('Error sending request: $e');
+    }
+  }
+
+  String _generateRandomCode() {
+    const chars = '0123456789';
+    final random = Random();
+    return String.fromCharCodes(Iterable.generate(
+        6, (_) => chars.codeUnitAt(random.nextInt(chars.length))));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("회원가입")),
-        body: Center(
-          child: Padding(
-            padding: EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextField(
-                  controller: _id,
-                  decoration: InputDecoration(
-                    labelText: "아이디",
+        body: SingleChildScrollView(
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextField(
+                    controller: _id,
+                    decoration: InputDecoration(
+                      labelText: "아이디",
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-                ),
-                SizedBox(height: 20),
-                TextField(
-                  controller: _name,
-                  decoration: InputDecoration(
-                    labelText: "이름",
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: _name,
+                    decoration: InputDecoration(
+                      labelText: "이름",
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-                ),
-                SizedBox(height: 20),
-                TextField(
-                  controller: _pwd,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: "비밀번호",
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: _pwd,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: "비밀번호",
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-                ),
-                SizedBox(height: 20),
-                TextField(
-                  controller: _pwd1,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: "비밀번호확인",
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: _pwd1,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: "비밀번호확인",
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-                ),
-                SizedBox(height: 20),
-                TextField(
-                  controller: _email,
-                  decoration: InputDecoration(
-                    labelText: "이메일",
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: _email,
+                    decoration: InputDecoration(
+                      labelText: "이메일",
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-                ),
-                SizedBox(height: 20),
-                TextField(
-                  controller: _phone,
-                  decoration: InputDecoration(
-                    labelText: "저나버노",
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: _phone,
+                    decoration: InputDecoration(
+                      labelText: "저나버노",
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: _register,
-                  child: Text("사용자 가입!"),
-                ),
-                SizedBox(height: 20),
-              ],
+                  SizedBox(height: 20),
+                  Row(
+                    children: [
+                      ElevatedButton(
+                        onPressed: sendSMS,
+                        child: Text('인증'),
+                      ),
+                      SizedBox(width: 20), // 추가할 텍스트 위젯
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: _register,
+                    child: Text("사용자 가입!"),
+                  ),
+                  SizedBox(height: 20),
+                ],
+              ),
             ),
           ),
         ),
       ),
     );
+
   }
 }
