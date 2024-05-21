33360018c1c96419c55610a41345db59a69e2ba1
KDH94 <gksanseldj@gmail.com>
2024-05-21 17:33:59 +0900

    240521 1733 / 채팅룸 콜렉션 및 메시지 서브콜렉션 생성과 채팅 기능 구현

--- lib/messenger/LoginPage.dart
+++ lib/messenger/LoginPage.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'ChatRoomListPage.dart';
 
@@ -11,43 +12,39 @@ class LoginPage extends StatelessWidget {
         title: const Text('로그인 페이지'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ChatRoomListPage(userId: 'user1'),
-                  ),
+        child: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance.collection('USERLIST').snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            }
+
+            if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+              return const Text('사용자가 없습니다.');
+            }
+
+            final users = snapshot.data!.docs;
+
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: users.map((user) {
+                final name = user['name'];
+                final userId = user.id;
+
+                return ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ChatRoomListPage(userId: userId),
+                      ),
+                    );
+                  },
+                  child: Text('$name(으)로 로그인'),
                 );
-              },
-              child: const Text('User1로 로그인'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ChatRoomListPage(userId: 'user2'),
-                  ),
-                );
-              },
-              child: const Text('User2로 로그인'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ChatRoomListPage(userId: 'user3'),
-                  ),
-                );
-              },
-              child: const Text('User3로 로그인'),
-            ),
-          ],
+              }).toList(),
+            );
+          },
         ),
       ),
     );
--- lib/messenger/tempChatMain.dart
+++ lib/messenger/tempChatMain.dart
@@ -2,19 +2,19 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
-import 'messenger/LoginPage.dart';
+// import 'messenger/LoginPage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  initializeDateFormatting().then((_) => runApp(const MainPage()));
+  initializeDateFormatting().then((_) => runApp(const MyApp()));
 }
 
-class MainPage extends StatelessWidget {
-  const MainPage({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) => MaterialApp(
-    home: const LoginPage(),
+// home: const LoginPage(),
   );
 }
