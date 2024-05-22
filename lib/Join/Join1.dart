f0490cb95209c9ab2a86c8adc2df87e6b0a68db9
jh <jih9117@naver.com>
2024-05-22 18:13:40 +0900

    240522 teamProject_join 정규식 추가

--- lib/Join/Join1.dart
+++ lib/Join/Join1.dart
@@ -5,6 +5,28 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
 
+bool isValidName(String name) {
+  String nameRegex = r'^[가-힣]{1,5}$|^[a-zA-Z]{1,10}$'; //한글로 1자에서 5자까지 또는 영어로 1자에서 10자까지
+  return RegExp(nameRegex).hasMatch(name);
+}
+
+bool isValidPassword(String password) {
+  String passwordRegex = r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
+  //최소 8자 이상이어야 하며, 대문자, 소문자, 숫자, 특수문자 중에서 4종류가 모두 포함되어야
+  return RegExp(passwordRegex).hasMatch(password);
+}
+
+bool isValidPhoneNumber(String phoneNumber) {
+  String phoneNumberRegex = r'^\d{9,11}$';// 9자리에서 11자리의 숫자
+  return RegExp(phoneNumberRegex).hasMatch(phoneNumber);
+}
+
+
+bool _isValidEmail(String email) {
+  return RegExp(r'^[a-zA-Z0-9][a-zA-Z0-9._%+-]*@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$').hasMatch(email);
+}//이메일 주소는 알파벳 또는 숫자로 시작해야 함, @ 다음에 도메인 이름
+
+
 class JoinEx2 extends StatefulWidget {
   const JoinEx2({Key? key});
 
@@ -18,10 +40,23 @@ class _JoinState extends State<JoinEx2> {
   final TextEditingController _pwd = TextEditingController();
   final TextEditingController _pwd1 = TextEditingController();
   final TextEditingController _email = TextEditingController();
+
+  void _showSnackBar(BuildContext context, String message) {
+    if (message.isNotEmpty) {
+      print(message);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(message)),
+      );
+    }
+  }
+
+
   final TextEditingController _phone = TextEditingController();
   final TextEditingController _todayDateController = TextEditingController();
 
   final TextEditingController _verificationCodeController = TextEditingController();
+  final String emailLabelText = "이메일";
+  final String emailHintText = "이메일을 입력하세요";
 
   @override
   void initState() {
@@ -41,24 +76,45 @@ class _JoinState extends State<JoinEx2> {
   bool _isVerified = false;
   String _snackBarMessage = '';
 
+
+  void _checkPhoneNumber() {
+    if (_phone.text.isEmpty) {
+      _showSnackBar(context,'전화번호를 입력하세요.');
+      return;
+    }
+
+    if (!isValidPhoneNumber(_phone.text)) {
+      _showSnackBar(context,'유효하지 않은 전화번호 형식입니다. 다시 입력하세요.');
+      return;
+    }
+  }
+
   void _register() async {
+
+
     if (!_isVerified) {
-      _showSnackBar('먼저 인증을 완료하세요.');
+      _showSnackBar(context,'먼저 인증을 완료하세요.');
+      return;
+    }
+
+    // 이름 유효성 검사 추가
+    if (!isValidName(_name.text)) {
+      _showSnackBar(context,'이름은 한글로 1자에서 5자까지 또는 영어로 1자에서 10자까지 입력하세요.');
+      return;
+    }
+
+    if (!isValidPassword(_pwd.text)) {
+      _showSnackBar(context,'비밀번호가 유효하지 않습니다. 비밀번호는 최소 8자 이상이어야 하며, 대문자, 소문자, 숫자, 특수문자 중에서 3종류 이상이 포함되어야 합니다.');
       return;
     }
 
     if (_pwd.text != _pwd1.text) {
-      _showSnackBar('패스워드가 다릅니다.');
+      _showSnackBar(context,'패스워드가 다릅니다.');
       return;
     }
 
     if (_pwd.text.isEmpty || _pwd.text != _pwd1.text) {
-      _showSnackBar('비밀번호를 확인하세요.');
-      return;
-    }
-
-    if (!_isValidEmail(_email.text)) {
-      _showSnackBar('올바른 이메일 주소를 입력하세요.');
+      _showSnackBar(context,'비밀번호를 확인하세요.');
       return;
     }
 
@@ -69,9 +125,10 @@ class _JoinState extends State<JoinEx2> {
         'email': _email.text,
         'phone': _phone.text,
         'todayDate': FieldValue.serverTimestamp(), // 서버 타임스탬프 사용
+        'imagePath': 'assets/place2.jpg',// 이미지 경로 추가
       });
 
-      _showSnackBar('가입되었음!!');
+      _showSnackBar(context,'가입되었음!!');
       _name.clear();
       _pwd.clear();
       _pwd1.clear();
@@ -79,13 +136,8 @@ class _JoinState extends State<JoinEx2> {
       _phone.clear();
       _verificationCodeController.clear();
     } catch (e) {
-      _showSnackBar('Error: $e');
+      _showSnackBar(context,'Error: $e');
     }
-  }
-
-  bool _isValidEmail(String email) {
-    String emailRegex = r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$';
-    return RegExp(emailRegex).hasMatch(email);
   }
 
   final String serverUrl = 'http://10.0.2.2:4000/send-sms';
@@ -134,7 +186,7 @@ class _JoinState extends State<JoinEx2> {
     print("enteredCode ==> $enteredCode");
     print("_generatedCode ==> $_generatedCode");
     if (enteredCode.isEmpty) {
-      _showSnackBar('인증 코드를 입력하세요.');
+      _showSnackBar(context,'인증 코드를 입력하세요.');
       return;
     }
 
@@ -142,9 +194,9 @@ class _JoinState extends State<JoinEx2> {
       setState(() {
         _isVerified = true;
       });
-      _showSnackBar('인증 완료되었습니다.');
+      _showSnackBar(context,'인증 완료되었습니다.');
     } else {
-      _showSnackBar('인증 코드가 다릅니다. 다시 시도해주세요.');
+      _showSnackBar(context,'인증 코드가 다릅니다. 다시 시도해주세요.');
     }
   }
 
@@ -155,36 +207,40 @@ class _JoinState extends State<JoinEx2> {
         6, (_) => chars.codeUnitAt(random.nextInt(chars.length))));
   }
 
-  void _showSnackBar(String message) {
-    print(message);
-    if (message.isNotEmpty) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text(message)),
-      );
-    }
-  }
-
   void _checkEmail() async {
     if (_email.text.isEmpty) {
-      _showSnackBar('이메일을 입력하세요.');
+      _showSnackBar(context, '이메일을 입력하세요.');
+      print(context);
+      return;
+    }
+
+    if (!_isValidEmail(_email.text)) {
+      _showInvalidEmailMessage();
       return;
     }
 
     try {
-      var checkEmail = await _fs
-          .collection('USERLIST')
-          .where('email', isEqualTo: _email.text)
-          .get();
+      var checkEmail = await _checkDuplicateEmail(_email.text);
 
       if (checkEmail.docs.isNotEmpty) {
-        _showSnackBar('이미 존재하는 이메일입니다.');
+        _showSnackBar(context,'이미 존재하는 이메일입니다.');
       } else {
-        _showSnackBar('사용 가능한 이메일입니다.');
+        _showSnackBar(context,'사용 가능한 이메일입니다.');
       }
     } catch (e) {
-      _showSnackBar('이메일 확인 중 오류가 발생했습니다: $e');
+      _showSnackBar(context,'이메일 확인 중 오류가 발생했습니다: $e');
     }
   }
+
+  void _showInvalidEmailMessage() {
+    _showSnackBar(context,'올바른 이메일 형식을 입력하세요.');
+  }
+
+  //사용자 목록을 조회하여 주어진 이메일과 일치하는 사용자를 찾는 역할
+  Future<QuerySnapshot> _checkDuplicateEmail(String email) async {
+    return await _fs.collection('USERLIST').where('email', isEqualTo: email).get();
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -192,7 +248,7 @@ class _JoinState extends State<JoinEx2> {
       home: Scaffold(
         backgroundColor: Color.fromARGB(255, 255, 253, 239),
         appBar: AppBar(
-          title: Text("회원가입"),
+          title: Text("회원가입", style: TextStyle(fontFamily: 'Pretendard', fontWeight: FontWeight.w500)),
           backgroundColor: Color.fromARGB(255, 255, 253, 239),
         ),
         body: Container(
@@ -203,33 +259,58 @@ class _JoinState extends State<JoinEx2> {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
+                    // TextField(//예전 버튼 따로 있던 코드
+                    //   style: TextStyle(fontFamily: 'Pretendard', fontWeight: FontWeight.bold),
+                    //   controller: _email,
+                    //   decoration: InputDecoration(
+                    //     labelText: "이메일",
+                    //     hintText: "이메일을 입력하세요",
+                    //     border: OutlineInputBorder(),
+                    //   ),
+                    // ),
+                    // SizedBox(height: 20),
+                    // Row(
+                    //   children: [
+                    //     ElevatedButton(
+                    //       onPressed: _checkEmail,
+                    //       style: ElevatedButton.styleFrom(
+                    //         backgroundColor: Color.fromRGBO(255, 219, 28, 1),
+                    //       ),
+                    //       child: Text(
+                    //         '중복 확인',
+                    //         style: TextStyle(
+                    //             color: Colors.black,
+                    //             fontFamily: 'Pretendard',
+                    //             fontWeight: FontWeight.bold), // 텍스트 색상을 검정색으로 변경
+                    //       ),
+                    //     ),
+                    //   ],
+                    // ),
                     TextField(
                       controller: _email,
                       decoration: InputDecoration(
                         labelText: "이메일",
                         hintText: "이메일을 입력하세요",
                         border: OutlineInputBorder(),
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    Row(
-                      children: [
-                        ElevatedButton(
+                        suffixIcon: ElevatedButton(
                           onPressed: _checkEmail,
                           style: ElevatedButton.styleFrom(
-                            backgroundColor: Color.fromRGBO(
-                                255, 219, 28, 1), // RGB 값으로 배경색 지정
+                            backgroundColor: Color.fromRGBO(255, 219, 28, 1),
                           ),
                           child: Text(
                             '중복 확인',
                             style: TextStyle(
-                                color: Colors.black), // 텍스트 색상을 검정색으로 변경
+                              color: Colors.black,
+                              fontFamily: 'Pretendard',
+                              fontWeight: FontWeight.w500,
+                            ),
                           ),
                         ),
-                      ],
+                      ),
                     ),
                     SizedBox(height: 20), // 여백 추가
                     TextField(
+                      style: TextStyle(fontFamily: 'Pretendard', fontWeight: FontWeight.bold),
                       controller: _name,
                       decoration: InputDecoration(
                         labelText: "이름",
@@ -238,7 +319,11 @@ class _JoinState extends State<JoinEx2> {
                     ),
                     SizedBox(height: 20),
                     TextField(
+                      style: TextStyle(fontFamily: 'Pretendard', fontWeight: FontWeight.bold),
                       controller: _pwd,
+                      onChanged: (value) {
+                        setState(() {}); // 입력이 변경될 때마다 화면을 다시 그려줌
+                      },
                       obscureText: true,
                       decoration: InputDecoration(
                         labelText: "비밀번호",
@@ -247,6 +332,7 @@ class _JoinState extends State<JoinEx2> {
                     ),
                     SizedBox(height: 20),
                     TextField(
+                      style: TextStyle(fontFamily: 'Pretendard', fontWeight: FontWeight.bold),
                       controller: _pwd1,
                       obscureText: true,
                       decoration: InputDecoration(
@@ -256,6 +342,7 @@ class _JoinState extends State<JoinEx2> {
                     ),
                     SizedBox(height: 20),
                     TextField(
+                      style: TextStyle(fontFamily: 'Pretendard', fontWeight: FontWeight.bold),
                       controller: _phone,
                       decoration: InputDecoration(
                         labelText: "전화번호",
@@ -266,7 +353,12 @@ class _JoinState extends State<JoinEx2> {
                     Row(
                       children: [
                         ElevatedButton(
-                          onPressed: sendSMS,
+                          onPressed: () {
+                            _checkPhoneNumber(); // 전화번호 유효성 확인
+                            if (_phone.text.isNotEmpty && isValidPhoneNumber(_phone.text)) {
+                              sendSMS(); // 유효성 확인 후 SMS 보내기
+                            }
+                          },
                           style: ElevatedButton.styleFrom(
                             backgroundColor: Color.fromRGBO(
                                 255, 219, 28, 1), // RGB 값으로 배경색 지정
@@ -282,6 +374,7 @@ class _JoinState extends State<JoinEx2> {
                     if (_isVerificationSent) ...[
                       SizedBox(height: 20),
                       TextField(
+                        style: TextStyle(fontFamily: 'Pretendard', fontWeight: FontWeight.bold),
                         controller: _verificationCodeController,
                         decoration: InputDecoration(
                           labelText: "인증 코드 입력",
