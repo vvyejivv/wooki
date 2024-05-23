cd629a9ce113178805c21d8936630f1ac4630880
lsss <ekfm6901@gmail.com>
2024-05-23 18:06:52 +0900

    20240523 - 아이디/비밀번호 찾기 기능, 앨범 업로드 작업중

--- lib/find/find_id2.dart
+++ lib/find/find_id2.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'search_id.dart';
+import 'package:provider/provider.dart';
+import 'dart:convert';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
   runApp(FindID2());
@@ -23,13 +28,115 @@ class FindIDPage extends StatefulWidget {
 }
 
 class _FindIDPageState extends State<FindIDPage> {
-  final TextEditingController phoneController = TextEditingController();
-  final TextEditingController codeController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
-  String foundID = ''; // 찾은 아이디를 저장할 변수
+  Future<void> _updatePwd(String email, String pwd) async {
+    FirebaseFirestore fs = FirebaseFirestore.instance;
+    CollectionReference users = fs.collection("USERLIST");
+
+    QuerySnapshot snap = await users.where('email', isEqualTo: email).get();
+    for (QueryDocumentSnapshot doc in snap.docs) {
+      await users.doc(doc.id).update({'pwd': pwd});
+    }
+  }
+
+  bool isValidPassword(String password) {
+    String passwordRegex = r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
+    // 최소 8자 이상이어야 하며, 대문자, 소문자, 숫자, 특수문자 중에서 4종류가 모두 포함되어야
+    return RegExp(passwordRegex).hasMatch(password);
+  }
+
+  void _showChangeDialog(BuildContext context, String email) {
+    final TextEditingController pwdController = TextEditingController();
+    final TextEditingController pwd2Controller = TextEditingController();
+    final scaffoldContext = context;
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("비밀번호 변경"),
+          content: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(
+                  controller: pwdController,
+                  decoration: InputDecoration(
+                    labelText: '새 비밀번호 입력',
+                    border: OutlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return '비밀번호를 입력해주세요.';
+                    }
+                    if (!isValidPassword(value)) {
+                      return '최소 8자, 대문자, 소문자, 숫자,\n특수문자를 포함해야 합니다.';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 10),
+                TextFormField(
+                  controller: pwd2Controller,
+                  decoration: InputDecoration(
+                    labelText: '새 비밀번호 확인',
+                    border: OutlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return '비밀번호 확인을 입력해주세요.';
+                    }
+                    if (value != pwdController.text) {
+                      return '비밀번호가 일치하지 않습니다.';
+                    }
+                    return null;
+                  },
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () async {
+                if (_formKey.currentState?.validate() ?? false) {
+                  try {
+                    await _updatePwd(email, pwdController.text);
+                    Navigator.of(context).pop();
+                    ScaffoldMessenger.of(scaffoldContext).showSnackBar(
+                      SnackBar(content: Text('비밀번호가 변경되었습니다.')),
+                    );
+                  } catch (error) {
+                    Navigator.of(context).pop();
+                    ScaffoldMessenger.of(scaffoldContext).showSnackBar(
+                      SnackBar(content: Text('비밀번호 변경에 실패했습니다.')),
+                    );
+                  }
+                }
+              },
+              child: Text("변경"),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("취소"),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
+    final searchId = Provider.of<SearchId>(context);
+    final email = searchId.find?.email;
     return Scaffold(
       appBar: AppBar(
         title: Text('아이디 찾기'),
@@ -46,16 +153,28 @@ class _FindIDPageState extends State<FindIDPage> {
               ),
               SizedBox(height: 20.0),
               Text(
-                '아이디: $foundID',
+                '아이디: $email',
                 style: TextStyle(fontSize: 18.0),
               ),
               SizedBox(height: 20.0),
-              ElevatedButton(
-                onPressed: () {
-                  // 로그인 버튼을 눌렀을 때의 동작 추가
-                  // 여기서는 여기서는 해당 동작을 추가하지 않았습니다.
-                },
-                child: Text('로그인'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      // 로그인 버튼을 눌렀을 때의 동작 추가
+                      // 여기서는 해당 동작을 추가하지 않았습니다.
+                    },
+                    child: Text('로그인'),
+                  ),
+                  SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      _showChangeDialog(context, email!);
+                    },
+                    child: Text('비밀번호 찾기'),
+                  ),
+                ],
               ),
             ],
           ),
--- lib/find/search_id.dart
+++ lib/find/search_id.dart
@@ -2,6 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class SearchId with ChangeNotifier {
+  Find? _find;
+  Find? get find => _find;
   bool _isSearch = false;
   bool get isSearch => _isSearch;
   final FirebaseFirestore _fs = FirebaseFirestore.instance;
@@ -11,12 +13,27 @@ class SearchId with ChangeNotifier {
         .where('phone', isEqualTo: phone)
         .get();
 
-    if (checkId.docs.isEmpty) {
+    if (checkId.docs.isNotEmpty) {
+      var doc = checkId.docs.first;
       _isSearch = true;
+      _find = Find(
+          email: doc['email'],
+          name: doc['name'],
+          phone: doc['phone']
+      );
     } else {
       _isSearch = false;
+      _find = null;
     }
 
     notifyListeners();
   }
+}
+
+class Find {
+  final String email;
+  final String name;
+  final String phone;
+
+  Find({required this.email, required this.name , required this.phone});
 }
